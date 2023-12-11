5da3ea509b64b01e3db8d42d26cd280418dd93ed
慕容云海 <zy94xhn.997@gmail.com>
2023-12-11 22:52:06 +0800

    deploy: set origin

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- 获取./imgs文件夹下的所有图片 -->
+<?php
+$dir = "./imgs";
+$files = scandir($dir);
+$files = array_diff($files, array('.', '..'));
+// 并且将文件拼接为 http://localhost:8080/imgs/xxx.jpg 的形式
+foreach ($files as &$file) {
+  $file = "http://119.91.237.233//imgs/" . $file;
+}
+$files = array_values($files);
+/**
+ * 返回的数据格式为
+ * {
+ *  code: 0,
+ * data: [
+ * "http://localhost:8080/imgs/1.jpg",
+ * "http://localhost:8080/imgs/2.jpg",
+ * ...}
+ * ]
+ * }
+ */
+$files = json_encode(array(
+  "code" => 0,
+  "data" => $files
+));
+// 返回标准的json格式
+header('Content-Type:application/json');
+echo $files;
+?>
+
